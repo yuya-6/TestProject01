080876218e7f6f7ebef8568b10bb5ae4fad6a500
yuya-6 <yuchongakufu@gmail.com>
2024-03-22 20:48:01 +0900

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		
+		public static String hello(){
+			return "こんにちは";
+		}
+				
+		System.out.println("山田さん。"+hello());
+		System.out.println("<br>");
+		System.out.println("鈴木さん。"+hello());
+		System.out.println("<br>");
+		System.out.println("佐藤さん。"+hello());
+	
+	}
+
+}
