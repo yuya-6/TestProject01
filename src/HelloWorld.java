50a5cbe842d5bda5a418ccca8d1622ec6244c67f
yuya-6 <yuchongakufu@gmail.com>
2024-03-24 21:01:45 +0900

    「山田さんこんにちは」追加

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,16 @@ public class HelloWorld {
 		
 		String abc = "山田";
 		System.out.println(abc+"さん");
+		
+		String hello="こんにちは";
+	  	System.out.println("山田さん。"+hello);
+	    System.out.println("鈴木さん。"+hello);
+	    System.out.println("佐藤さん。"+hello);
 	
 	}
-
+	
 }
+
+
+
+
